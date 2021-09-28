8ad925b21a2fe4acf235eae842641a3ed5851026
nadjajovanovic <j.nadja97@gmail.com>
2021-09-28 14:28:42 +0200

    Modified TimeSheetApplication beacuse it did not run

--- TimeSheet/src/main/java/projekat/TimeSheetApplication.java
+++ TimeSheet/src/main/java/projekat/TimeSheetApplication.java
@@ -5,10 +5,20 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@Configuration
+@EntityScan
+@ComponentScan({"projekat"})
+@EnableJpaRepositories(basePackages = "projekat.models")
+@EnableTransactionManagement
 public class TimeSheetApplication {
 
 	public static void main(String[] args) {
