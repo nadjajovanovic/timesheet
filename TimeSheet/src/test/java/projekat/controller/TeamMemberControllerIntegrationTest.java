273c0ca788823b1566859a3041893b706e8f75ef
Nadja Jovanovic <j.nadja97@gmail.com>
2021-11-02 11:51:10 +0100

    TeamMember test modified

--- TimeSheet/src/test/java/projekat/controller/TeamMemberControllerIntegrationTest.java
+++ TimeSheet/src/test/java/projekat/controller/TeamMemberControllerIntegrationTest.java
@@ -1,11 +1,13 @@
 package projekat.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.checkerframework.common.value.qual.ArrayLenRange;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.postgresql.util.MD5Digest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import projekat.TimeSheetApplication;
 import projekat.models.Client;
 import projekat.models.Teammember;
@@ -23,16 +24,21 @@ import projekat.util.ResponseReader;
 
 import java.util.Arrays;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.test.web.servlet.MvcResult;
+
+import javax.print.attribute.standard.Media;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TimeSheetApplication.class)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
-public class TeamMemberControllerIntegrationTest {
+class TeamMemberControllerIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
@@ -51,68 +57,69 @@ public class TeamMemberControllerIntegrationTest {
     public void doCleanDatabase() {
         cleanDataBase();
     }
-    
+
     @Test
     public void getAllTeamMembers() throws Exception {
-        //Arange
-        final var firstTeamMemberName = "First";
-        createTestTeamMember(firstTeamMemberName);
+        //Arrange
+        final var teamMemberName = "First";
+        createTeamMember(teamMemberName);
 
-        //Act
+        //act
         final var response = mvc.perform(get("/teammember")
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
         final var teamMembers = Arrays.asList(ResponseReader.readResponse(response, Teammember[].class));
 
         //Assert
-       assertEquals(teamMembers.size(), 1);
-       assertEquals(teamMembers.get(0).getTeammembername(), firstTeamMemberName);
+        assertEquals(teamMembers.size(), 1);
+        assertEquals(teamMembers.get(0).getTeammembername(), teamMemberName);
     }
 
+    //@org.junit.Test
     @Test
     public void getOneTeamMember() throws Exception {
-        //Arange
-        final var teamMemberNAme = "First";
-        final var inserted = createTestTeamMember(teamMemberNAme);
+        //Arrange
+        final var teamMemberName = "First";
+        final var inserted = createTeamMember(teamMemberName);
 
         //Act
-        final var response = mvc.perform(get("/teammember/{teammemberid}", inserted.getTeammemberid())
+        final var response = mvc.perform(get("/teammember/{id}", inserted.getTeammemberid())
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
-        final var teammember = ResponseReader.readResponse(response, Teammember.class);
+        final var teamMember = ResponseReader.readResponse(response, Teammember.class);
 
         //Assert
-        assertEquals(teammember.getTeammembername(), teamMemberNAme);
-        assertEquals(teammember.getTeammemberid(), Integer.valueOf(inserted.getTeammemberid()));
+        assertEquals(teamMember.getTeammembername(), teamMemberName);
+        assertEquals(teamMember.getTeammemberid(), Integer.valueOf(inserted.getTeammemberid()));
     }
 
     @Test
     public void getOneTeamMemberNotFound() throws Exception {
         //Arange
-        final var teammemberId = "100";
+        final var teamMemberId = "100";
 
-        //act
-        final var response = mvc.perform(get("/teammember/{teammemberid}", teammemberId)
+        //Act
+        final var response = mvc.perform(get("/teammember/{teamMemberId}", teamMemberId)
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn();
 
-        //Assert
+        //assert
         assertEquals(response.getResponse().getStatus(), HttpStatus.NOT_FOUND.value());
     }
 
-    /*@Test
-    public void testTeamMember() throws Exception {
-        //Arrange
+    @Test
+    public void testCreateTeamMember() throws Exception {
+        //Arange
         final var teamMemberName = "Please insert me";
-        final var teammember = new Teammember();
-        teammember.setTeammembername(teamMemberName);
+        final var teamMember = new Teammember();
+        teamMember.setTeammembername(teamMemberName);
 
         //Act
         final var response = mvc.perform(post("/teammember")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(teammember))
+                        .content(objectMapper.writeValueAsString(teamMember))
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andReturn();
@@ -121,29 +128,150 @@ public class TeamMemberControllerIntegrationTest {
         //Assert
         assertNotNull(responseTeamMember.getTeammemberid());
         assertEquals(responseTeamMember.getTeammembername(), teamMemberName);
-    }*/
+    }
 
     @Test
     public void testCreateTeamMemberBadRequest() throws Exception {
         //Arange
-        final var teammember = new Teammember();
-        teammember.setTeammembername("");
+        final var teamMember = new Teammember();
+        teamMember.setTeammembername("");
 
         //Act
         final var response = mvc.perform(post("/teammember")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(teammember))
+                        .content(objectMapper.writeValueAsString(teamMember))
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn();
 
         //Assert
         assertEquals(response.getResponse().getStatus(), HttpStatus.BAD_REQUEST.value());
     }
-    
-    private Teammember createTestTeamMember(String teamMemberName) {
-        final var teamMember  = new Teammember();
+
+    @Test
+    public void testCreateTeamMemberNameNotExist() throws Exception {
+        //Arange
+        final var teamMember = new Teammember();
+
+        //Act
+        final var response = mvc.perform(post("/teammember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(teamMember))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void testCreateTeamMemberIdExists() throws Exception {
+        //Arange
+        final var teamMemberName = "Please insert me";
+        final var teamMember = new Teammember();
         teamMember.setTeammembername(teamMemberName);
-        return repository.saveAndFlush(teamMember);
+        teamMember.setTeammemberid(5);
+
+        //Act
+        final var response = mvc.perform(post("/teammember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(teamMember))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        //assert
+        assertEquals(response.getResponse().getStatus(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void testUpdateTeamMember() throws Exception {
+        //Arange
+        final var teamMemberName = "nameForInsert";
+        final var inserted = createTeamMember(teamMemberName);
+        final var updatedName = "nameForUpdate";
+        inserted.setTeammembername(updatedName);
+
+        //Act
+        final var response = mvc.perform(put("/teammember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inserted))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        final var responseTeamMember = ResponseReader.readResponse(response, Teammember.class);
+
+        //Assert
+        assertNotNull(responseTeamMember.getTeammemberid());
+        assertEquals(responseTeamMember.getTeammembername(), updatedName);
+    }
+
+    @Test
+    public void testUpdateTeamMemberBadRequest() throws Exception {
+        //Arange
+        final var teamMemberName = "NameForInsert";
+        final var inserted = createTeamMember(teamMemberName);
+        final var updatedName = "";
+        inserted.setTeammembername(updatedName);
+
+        //Act
+        final var response = mvc.perform(put("/teammember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inserted))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        //Assert
+        assertEquals(response.getResponse().getStatus(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test @Disabled
+    public void testUpdateTeamMemberNoId() throws Exception {
+        //Arange
+        final var teamMember = new Teammember();
+        teamMember.setTeammembername("Not important");
+
+        //act
+        final var response = mvc.perform(put("/teammember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(teamMember))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        //Assert
+        assertEquals(response.getResponse().getStatus(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void deleteTeamMember() throws Exception {
+        //Arange
+        final var teamMemberName = "Delete me";
+        final var inserted = createTeamMember(teamMemberName);
+
+        //act
+        final var response = mvc.perform(delete("/teammember/{teammemberid}", inserted.getTeammemberid())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        //Assert
+        assertEquals(response.getResponse().getStatus(), HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deleteTeamMemberNotFound() throws Exception {
+        //Arange
+        final var teamMemberId = "100";
+
+        //act
+        final var response = mvc.perform(delete("/teammember/{teammemberid}", teamMemberId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        //assert
+        assertEquals(response.getResponse().getStatus(), HttpStatus.NOT_FOUND.value());
+    }
+
+    private Teammember createTeamMember(String teammemberName) {
+        final var teammember = new Teammember();
+        teammember.setTeammembername(teammemberName);
+        return repository.saveAndFlush(teammember);
     }
 
     private void cleanDataBase() {
