f1288ec6e74ed0eb23a0122ec11409fd9ad02fa2
Dusan Zurkovic <d.zurkovic@vega.local>
2021-11-10 17:30:23 +0100

    Add rest method for TeamMemmber in OpenApi.yaml file

--- TimeSheet/src/main/java/projekat/TimeSheetApplication.java
+++ TimeSheet/src/main/java/projekat/TimeSheetApplication.java
@@ -2,7 +2,6 @@ package projekat;
 
 import java.util.Arrays;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
