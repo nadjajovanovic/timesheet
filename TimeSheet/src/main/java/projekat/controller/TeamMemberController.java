56efe39d88baf872909ad10f8c5fa979f7f34b81
Dusan Zurkovic <d.zurkovic@vega.local>
2021-11-16 11:06:44 +0100

    Resolve the conflict

--- TimeSheet/src/main/java/projekat/controller/TeamMemberController.java
+++ TimeSheet/src/main/java/projekat/controller/TeamMemberController.java
@@ -1,11 +1,6 @@
 package projekat.controller;
 
 
-import java.util.Collection;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
