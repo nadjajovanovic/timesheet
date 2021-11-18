e8990a0c58b47e6f053606a3109d612bc5e97301
BojanaZoric <bjnzrc@gmail.com>
2021-11-18 14:08:57 +0100

    Excel report endpoint done

--- TimeSheet/src/main/java/projekat/controller/ReportController.java
+++ TimeSheet/src/main/java/projekat/controller/ReportController.java
@@ -1,7 +1,11 @@
 package projekat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +14,11 @@ import projekat.api.model.ReportFilterDTO;
 import projekat.api.model.TimeSheetEntryDTO;
 import projekat.mapper.TimeSheetEntryMapper;
 import projekat.services.ReportService;
+import projekat.util.ReportExcelExporter;
 
+import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -32,5 +40,30 @@ public class ReportController implements ReportApi {
 				.map(TimeSheetEntryMapper::toEntryForReportDTO)
 				.toList();
 		return new ResponseEntity(filtered, HttpStatus.OK);
+	}
+
+
+	@Override
+	public ResponseEntity<Resource> getExcelReport(ReportFilterDTO reportFilterDTO) {
+		final var resource = new ByteArrayOutputStream();
+		final var headers = new HttpHeaders();
+		final var dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		final var currentDateTime = dateFormatter.format(new Date());
+		final var headerKey = "Content-Disposition";
+		final var headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+		headers.add(headerKey, headerValue);
+		headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+		final var generatedReports = reportService.generateReport(reportFilterDTO);
+		final var filtered = generatedReports
+				.stream()
+				.map(TimeSheetEntryMapper::toEntryForReportDTO)
+				.toList();
+
+		final var excelExporter = new ReportExcelExporter(filtered);
+		excelExporter.export(resource);
+		final var res = new ByteArrayResource(resource.toByteArray());
+
+		return new ResponseEntity( res, headers, HttpStatus.OK);
 	}
 }
