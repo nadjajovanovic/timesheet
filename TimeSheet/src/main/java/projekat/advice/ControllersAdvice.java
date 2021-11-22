d586652dac7c0b841c7cbff9815065a8bc407d69
Dusan Zurkovic <d.zurkovic@vega.local>
2021-11-22 13:46:02 +0100

    Resolve the comment

--- TimeSheet/src/main/java/projekat/advice/ControllersAdvice.java
+++ TimeSheet/src/main/java/projekat/advice/ControllersAdvice.java
@@ -19,21 +19,21 @@ public class ControllersAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleApiException(NoSuchElementException e) {
         final var exception = new ErrorResponse(ErrorCode.NOT_FOUND.toString(),"Element with that id not exist in DB",HttpStatus.NOT_FOUND.value());
-        log.error(exception.toString());
+        log.error("API error! errorCode: {}", exception.getErrorCode(), e);
         return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
         final var error = new ErrorResponse(e);
-        log.error(error.toString());
+        log.error("API error! errorCode: {}", e.getErrorCode(), e);
         return new ResponseEntity<>(error, e.getHttpStatus());
     }
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(ApiException e) {
         final var error = new ApiException(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR,e.getCause());
-        log.error(error.toString());
+        log.error("API error! errorCode: {}", e.getErrorCode(), e);
         return handleApiException(error);
     }
 }
